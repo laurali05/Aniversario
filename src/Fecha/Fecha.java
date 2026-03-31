d88f2cbe88cd622a4ab6e4ee360acd219a79ff69
Laura Lope <lopeizquierd@gmail.com>
2026-03-31 17:25:38 +0200

    Clase Fecha

--- src/Fecha/Fecha.java
+++ src/Fecha/Fecha.java
@@ -1,285 +1,31 @@
 package Fecha;
 
+import java.time.LocalDate;
+
 public class Fecha {
+	private LocalDate aniversario;
+	private LocalDate hoy;
 
-	private int dia;
-	private int mes;
-	private int any;
-
-	/**
-	 * Constructor sin parámetros
-	 */
-	public Fecha() {
-		this.dia = 3;
-		this.mes = 10;
-		this.any = 2026;
+	public Fecha (int año, int mes, int dia) {
+		this.aniversario = LocalDate.of(año, mes, dia);
+		this.hoy = LocalDate.now();
 	}
 
-	/**
-	 * Constructor con parámetros
-	 * 
-	 * @param dia
-	 * @param mes
-	 * @param any
-	 */
-	public Fecha(int dia, int mes, int any) {
-		if (esDataCorrecta(dia, mes, any)) {
-			this.dia = dia;
-			this.mes = mes;
-			this.any = any;
-		} else {
-			this.dia = 1;
-			this.mes = 1;
-			this.any = 2000;
-		}
-	}
-
-	/**
-	 * Getter
-	 * 
-	 * @return dia de la data
-	 */
-	public int getDia() {
-		return dia;
-	}
-
-	/**
-	 * Getter
-	 * 
-	 * @return mes de la data
-	 */
-	public int getMes() {
-		return mes;
-	}
-
-	/**
-	 * Getter
-	 * 
-	 * @return any de la data
-	 */
-	public int getAny() {
-		return any;
-	}
-
-	/**
-	 * Setter
-	 * 
-	 * @param dia
-	 * @param mes
-	 * @param any
-	 */
-	public void setData(int dia, int mes, int any) {
-		if (esDataCorrecta(dia, mes, any)) { // ens asegurem que hi ha una data vàlida
-			this.dia = dia;
-			this.mes = mes;
-			this.any = any;
-		}
-	}
-
-	// /**
-	//  * Método que comprueba si dos fechas son iguales
-	//  * 
-	//  * @param data - fecha a comparar
-	//  * @return si son iguales
-	//  */
-	// public boolean esIgual(Fecha data) {
-	// 	if (this.dia == data.getDia() && this.mes == data.getMes() && this.any == data.getAny()) {
-	// 		return true;
-	// 	}
-	// 	return false;
-	// }
-
-	/**
-	 * Mètode que calcula el dia siguiente a la fecha actual
-	 * * @return
-	 */
-	public Fecha diaSeguent() {
-		Fecha novaData = new Fecha(dia, mes, any);
-
-		novaData.dia++;
-		if (novaData.dia > diesMes(novaData.mes, novaData.any)) {
-			novaData.dia = 1;
-			novaData.mes++;
-			if (novaData.mes > 12) {
-				novaData.mes = 1;
-				novaData.any++;
-			}
-		}
-		return novaData;
-	}
-
-	// /**
-	//  * 
-	//  * Método que calcula el día anterior a la fecha actual
-	//  */
-	// public void diaAnterior() {
-	// 	this.dia--;
-	// 	if (this.dia < 1) {
-	// 		this.mes--;
-	// 		if (this.mes < 1) {
-	// 			this.mes = 12;
-	// 			this.any--;
-	// 		}
-	// 		this.dia = diesMes(this.mes, this.any);
-	// 	}
-	// }
-
-	// /**
-	//  * Método que calcula cuanto falta para una fecha introducida por parámetro
-	//  * 
-	//  * @param data
-	//  * @return
-	//  */
-	// public int numDiesAData(Fecha data) {
-	// 	Fecha dataTemp;
-	// 	int contador;
-	// 	dataTemp = new Fecha(dia, mes, any);
-	// 	if (dataTemp.esDataInferiorOigual(data)) {
-	// 		contador = 0;
-	// 		// la data rebuda és major que l'actual
-	// 		while (!dataTemp.esIgual(data)) {
-	// 			dataTemp = dataTemp.diaSeguent();
-	// 			contador++;
-	// 		}
-	// 	} else
-	// 		contador = -1;
-	// 	return contador;
-	// }
-
-	/**
-	 * Método que comprueba si una fecha es inferior a otra
-	 * 
-	 * @param nova
-	 * @return
-	 */
-	public boolean esDataInferiorOigual(Fecha nova) {
-		boolean esInferior;
-		if (any < nova.any)
-			esInferior = true;
-		else if (any > nova.any)
-			esInferior = false;
-		else {
-			// cas anys iguals
-			if (mes < nova.mes)
-				esInferior = true;
-			else if (mes > nova.mes)
-				esInferior = false;
-			else {
-				// anys i mes iguals
-				if (dia <= nova.dia)
-					esInferior = true;
-				else
-					esInferior = false;
-			}
-		}
-		return esInferior;
-
-	}
-
-	/**
-	 * Método que comprueba si la fecha actual pertenece a un año bisiesto
-	 * 
-	 * @return
-	 */
-	public boolean esDataAnyTraspas() {
-		if (esAnyTraspas(this.any)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	@Override
-	public String toString() {
-		return ("[" + dia + ", " + mes + ", " + any + "]");
-	}
-
-	/**
-	 * Método que crea un duplicado de la fecha actual
-	 * 
-	 * @return
-	 */
-	public Fecha copia() {
-		Fecha aux = new Fecha(dia, mes, any);
-		return aux;
-	}
-
-	/**
-	 * Suma x días a la fecha actual 
-	 * Autor Laura Lope
-	 * 
-	 * @param dias
-	 * @return
-	 */
-	public Fecha sumarDias(int dias){
-		Fecha nuevaData = new Fecha(this.dia, this.mes, this.any);
-
-		for (int i = 0; i < dias; i++){
-			nuevaData = nuevaData.diaSeguent();
-		}
-
-		return nuevaData;
-	}
-
-	
-
-	private static boolean esDataCorrecta(int dia, int mes, int any) {
-		boolean hoEs = true;
-		if (dia < 1 || dia > 31) {
-			hoEs = false;
-		}
-		if (mes < 1 || mes > 12) {
-			hoEs = false;
-		}
-		if (dia > diesMes(mes, any)) {
-			hoEs = false;
-		}
-		return hoEs;
-	}
-
-	public static boolean esAnyTraspas(int any) {
-		if ((any % 4 == 0) && ((any % 100 != 0) || (any % 400 == 0))) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public static int diesMes(int mes, int any) {
-		int diesMes;
-		if (mes == 2) {
-			if (esAnyTraspas(any)) {
-				diesMes = 29;
+	public int diasEntreFechas(){
+		int dias = -1;
+		if((aniversario.getYear() == hoy.getYear()) && (aniversario.getDayOfYear() == hoy.getDayOfYear())){
+			dias = 0;
+		} else if ((aniversario.getYear() == hoy.getYear()) && (aniversario.getDayOfYear() != hoy.getDayOfYear())){
+			if(hoy.getDayOfYear() > aniversario.getDayOfYear()){
+				dias = hoy.getDayOfYear() - aniversario.getDayOfYear();
 			} else {
-				diesMes = 28;
+				dias = -1;
+				System.err.println("Error, el aniversario aún no ha tenido lugar");
 			}
-		} else {
-			if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
-				diesMes = 30;
-			} else {
-				diesMes = 31;
-			}
+		} else if ((aniversario.getYear() != hoy.getYear())){
+			dias = (aniversario.lengthOfYear() - aniversario.getDayOfYear()) + hoy.getDayOfYear();
 		}
-		return diesMes;
+		return dias;
 	}
-
-	public static int calcularDiaSetmana(int dia, int mes, int any) {
-        int m = mes;
-        int a = any;
-
-        if (m < 3) {
-            m += 12;
-            a -= 1;
-        }
-
-        int K = a % 100;
-        int J = a / 100;
-
-        int h = (dia + 13 * (m + 1) / 5 + K + K / 4 + J / 4 + 5 * J) % 7;
-
-        int resultat = ((h + 5) % 7) + 1;
-
-        return resultat - 1;
-    }
-
 }
+
