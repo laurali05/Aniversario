caa78ff1efe529c590f3e3a2000f10874e42146a
Laura Lope <lopeizquierd@gmail.com>
2026-03-31 18:05:21 +0200

    Cambios previos a la web

--- src/Archivos/Lector.java
+++ src/Archivos/Lector.java
@@ -1,5 +1,12 @@
 package Archivos;
 
+import java.util.Scanner;
+import java.io.File;
+
 public class Lector {
+
+    public void leerCarta(int dia){
+        String ruta = "src/Archivos/Correos/" + dia + ".txt";
+    }
     
 }
