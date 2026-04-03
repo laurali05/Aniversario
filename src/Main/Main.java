8e3d6213ba7e118d8f86f4b80b2a218a7a87b618
Laura Lope <lopeizquierd@gmail.com>
2026-04-03 18:50:53 +0200

    Cambios

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,32 +1,8 @@
 package Main;
 
-import java.io.File;
-import java.io.IOException;
+
 
 public class Main {
     // Todo programa Java empieza a ejecutar aquí
-    public static void main(String[] args) {
-
-        // 1. Definimos la ruta exacta (con src/)
-        String rutaCarpeta = "src/Archivos/Correos/";
-
-        File directorio = new File(rutaCarpeta);
-        if (!directorio.exists()) {
-            directorio.mkdirs();
-        }
-
-        for (int i = 1; i <= 365; i++) {
-            // 2. Usamos la misma ruta para el archivo
-            File archivo = new File(rutaCarpeta + i + ".txt");
-            try {
-                if (archivo.createNewFile()) {
-                    System.out.println("Creado en src: " + i + ".txt");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("¡Proceso finalizado!");
-    }
+   
 }
