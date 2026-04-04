f6f855732be463096cd9b20d6968f0c974c2cce2
Laura Lope <lopeizquierd@gmail.com>
2026-04-04 11:46:30 +0200

    Errores

--- src/Archivos/Lector.java
+++ src/Archivos/Lector.java
@@ -8,7 +8,7 @@ public class Lector {
     public String leerCuerpoDelDia(int dia) {
         try {
             // Asegúrate de que esta carpeta existe en la raíz de tu proyecto
-            Path ruta = Paths.get("mensajes_email/" + dia + ".txt");
+            Path ruta = Paths.get("Correos/" + dia + ".txt");
             return Files.readString(ruta);
         } catch (IOException e) {
             return "¡Hola amor! Tienes una nueva carta esperándote en nuestra web. ❤️";
